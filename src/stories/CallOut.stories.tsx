c42280e5a6c7fe050d5d130144a8a2b44ec833c9
piri-m <piri-m@sayancard.ir>
2024-11-09 13:39:09 +0330

    add cllout component

---
+++ src/stories/CallOut.stories.tsx
@@ -0,0 +1,77 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Callout } from "../components/CallOut";
+
+const meta = {
+  title: "Callout",
+  component: Callout,
+  tags: ["autodocs"],
+  args: {
+    title: "An Important Message",
+    children: "This is a message that you should read."
+  },
+  argTypes: {
+    title: {
+      name: "Title",
+      control: "text",
+      description: "Title of the callout"
+    },
+    children: {
+      name: "Content",
+      control: "text",
+      description: "Content of the callout"
+    },
+    type: {
+      name: "Type",
+      control: "select",
+      options: [
+        "default",
+        "primary",
+        "information",
+        "success",
+        "warning",
+        "danger"
+      ],
+      description: "Type of callout",
+      table: {
+        defaultValue: {
+          summary: "default"
+        }
+      }
+    }
+  }
+} satisfies Meta<typeof Callout>;
+
+export default meta;
+type Story = StoryObj<typeof Callout>;
+
+export const Default: Story = {};
+
+export const Primary: Story = {
+  args: {
+    type: "primary"
+  }
+};
+
+export const Information: Story = {
+  args: {
+    type: "information"
+  }
+};
+
+export const Success: Story = {
+  args: {
+    type: "success"
+  }
+};
+
+export const Warning: Story = {
+  args: {
+    type: "warning"
+  }
+};
+
+export const Danger: Story = {
+  args: {
+    type: "danger"
+  }
+};
