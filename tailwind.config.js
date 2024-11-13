a709ce1cc002b24dd86832b96f5421f12caae662
piri-m <piri-m@sayancard.ir>
2024-11-13 10:18:30 +0330

    debug tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,23 @@
 /** @type {import('tailwindcss').Config} */
-import {
-  colors,
-  white,
-  black,
-  transparent,
-  currentColor
-} from './src/components/ColorPalette/ColorPalette';
+// import {
+//   colors,
+//   white,
+//   black,
+//   transparent,
+//   currentColor
+// } from './src/components/ColorPalette/ColorPalette';
 
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     theme: {
-      colors: {
-        ...colors,
-        white,
-        black,
-        transparent,
-        currentColor
-      }
+      // colors: {
+      //   ...colors,
+      //   white,
+      //   black,
+      //   transparent,
+      //   currentColor
+      // }
     },
     plugins: []
   }
